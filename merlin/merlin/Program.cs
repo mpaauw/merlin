2b812cb03c0cb730bfcbac230036c66317103751
mpaauw <matt.paauw@protonmail.com>
2021-01-03 12:54:12 -0800

    Add VS boilerplate.

---
+++ merlin/merlin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace merlin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
